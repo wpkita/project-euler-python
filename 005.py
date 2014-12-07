172ab22487e045854fd96b93514674bc7ec7d22a
wpkita <william.p.kita@gmail.com>
2014-12-07 00:18:05 -0800

    Add brute-force problem 5 solution

--- 005.py
+++ 005.py
@@ -1,1 +1,31 @@
-__author__ = 'william'
+"""
+Title
+=====
+Smallest multiple
+
+Problem
+=======
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+
+def is_divisible_by_consecutive_numbers(number, minimum, maximum):
+    for i in range(minimum, maximum + 1):
+        if number % i != 0:
+            return False
+
+    return True
+
+have_not_found_a_match = True
+current_number = 1
+
+while have_not_found_a_match:
+    if is_divisible_by_consecutive_numbers(current_number, 2, 20):
+        have_not_found_a_match = False
+    else:
+        current_number += 1
+
+print('Smallest multiple: ', current_number)
+
