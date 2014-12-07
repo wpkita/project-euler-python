3e9f5ec6cddbaea5cbfbe2995c5629ebcd3cfb51
wpkita <william.p.kita@gmail.com>
2014-12-07 00:21:53 -0800

    Improve efficiency by stepping 20 with each number to test

--- 005.py
+++ 005.py
@@ -18,14 +18,16 @@ def is_divisible_by_consecutive_numbers(number, minimum, maximum):
 
     return True
 
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 20
+
 have_not_found_a_match = True
-current_number = 1
+current_number = MAXIMUM_NUMBER
 
 while have_not_found_a_match:
-    if is_divisible_by_consecutive_numbers(current_number, 2, 20):
+    if is_divisible_by_consecutive_numbers(current_number, MINIMUM_NUMBER + 1, MAXIMUM_NUMBER):
         have_not_found_a_match = False
     else:
-        current_number += 1
+        current_number += MAXIMUM_NUMBER
 
 print('Smallest multiple: ', current_number)
-
