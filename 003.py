db4f25e9f1aed89907da0c1a0ca63f1dd51c6d76
wpkita <william.p.kita@gmail.com>
2014-12-06 21:12:26 -0800

    Add recursive problem 3 solution

--- 003.py
+++ 003.py
@@ -10,19 +10,31 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-from math import floor
+
+def get_factors_rec(number, factors):
+    for i in range(2, number // 2):
+        if number % i == 0:
+            factors.append(i)
+            get_factors_rec(number // i, factors)
+            return
+
+    # If we got this far, then this is a prime factor
+    factors.append(number)
+
+
+def get_factors(number):
+    factors = []
+
+    get_factors_rec(number, factors)
+
+    return factors
 
 
 def get_largest_prime_factor(number):
-    # Begin at the halfway point, since no factors will be greater than that
-    mid = floor(number / 2)
+    factors = get_factors(number)
 
-    # Recursively traverse factor tree
-    for i in range(mid, 1, -1):
-        if number % i == 0:
-            return get_largest_prime_factor(i)
+    return max(factors)
 
-    return number
 
 largest_prime_factor = get_largest_prime_factor(600851475143)
 
