04481134fdb7b2740f29220c7d3811c78e320779
wpkita <william.p.kita@gmail.com>
2014-12-11 15:15:46 -0800

    Solve p007

---
+++ p007/solution.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def generate_nth_prime(n):
+    prime_count = 0
+    current_number = 1
+
+    while prime_count < n:
+        current_number += 1
+
+        if is_prime(current_number):
+            prime_count += 1
+
+    return current_number
---
+++ tests/p007_tests.py
@@ -0,0 +1,16 @@
+from nose.tools import *
+from p007.solution import *
+
+
+def test_is_prime():
+    assert_true(is_prime(2))
+    assert_true(is_prime(3))
+    assert_false(is_prime(4))
+    assert_true(is_prime(5))
+    assert_false(is_prime(6))
+
+
+def test_generate_nth_prime():
+    assert_equal(generate_nth_prime(1), 2)
+    assert_equal(generate_nth_prime(2), 3)
+    assert_equal(generate_nth_prime(6), 13)
