25458d2a809c7ee0ae336ac3c698e8b7c47de8a2
wpkita <william.p.kita@gmail.com>
2014-12-11 00:44:03 -0800

    Add run script for p005

--- p005/solution.py
+++ p005/solution.py
@@ -17,17 +17,3 @@ def is_divisible_by_consecutive_numbers(number, minimum, maximum):
             return False
 
     return True
-
-MINIMUM_NUMBER = 1
-MAXIMUM_NUMBER = 20
-
-have_not_found_a_match = True
-current_number = MAXIMUM_NUMBER
-
-while have_not_found_a_match:
-    if is_divisible_by_consecutive_numbers(current_number, MINIMUM_NUMBER + 1, MAXIMUM_NUMBER):
-        have_not_found_a_match = False
-    else:
-        current_number += MAXIMUM_NUMBER
-
-print('Smallest multiple: ', current_number)
