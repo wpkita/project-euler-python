a28ee8fdd4b835d755aa101db895abf9f0b923c4
wpkita <william.p.kita@gmail.com>
2014-12-11 12:55:05 -0800

    Refactor evens logic for p002 into separate function

--- p002/solution.py
+++ p002/solution.py
@@ -19,11 +19,12 @@ def fibonacci_numbers_up_to(max_num):
 
     return seq
 
+
+def sum_of_evens(numbers):
+    return sum(i for i in numbers if i % 2 == 0)
+
 if __name__ == '__main__':
     fibs_not_exceeding_four_million = fibonacci_numbers_up_to(4000000)
-    sum_of_even_fibs_not_exceeding_four_million = 0
-    for i in fibs_not_exceeding_four_million:
-        if i % 2 == 0:
-            sum_of_even_fibs_not_exceeding_four_million += i
+    sum_of_even_fibs_not_exceeding_four_million = sum_of_evens(fibs_not_exceeding_four_million)
 
     print('Sum of even Fibonacci numbers not exceeding four million: ', sum_of_even_fibs_not_exceeding_four_million)
--- tests/p002_tests.py
+++ tests/p002_tests.py
@@ -6,3 +6,9 @@ def test_fibonacci_sequence():
     fib_numbers_up_to_10 = fibonacci_numbers_up_to(10)
 
     assert_equal(fib_numbers_up_to_10, [1, 2, 3, 5, 8])
+
+
+def test_sum_of_evens():
+    assert_equal(sum_of_evens([1, 2, 3, 4]), 6)
+    assert_equal(sum_of_evens([7, 13, 19]), 0)
+    assert_equal(sum_of_evens([]), 0)
