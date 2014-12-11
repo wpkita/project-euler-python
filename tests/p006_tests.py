3b4a1d67e7730506d4bb667e42e8f91a6cb095d1
wpkita <william.p.kita@gmail.com>
2014-12-11 12:34:51 -0800

    Fix typo in p006 test

--- tests/p006_tests.py
+++ tests/p006_tests.py
@@ -9,6 +9,6 @@ def test_sum_of_squares():
 
 
 def test_square_of_sums():
-    square_of_sums_up_to_10 = square_of_sums(10)
+    square_of_sums_up_to_10 = square_of_sum(10)
 
     assert_equal(square_of_sums_up_to_10, 3025)
