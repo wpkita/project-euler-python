01f9dca149067dfae4e30ec63eeb687ebf1dea29
wpkita <william.p.kita@gmail.com>
2014-12-11 00:50:06 -0800

    Use list comprehension to solve p001

--- p001/solution.py
+++ p001/solution.py
@@ -19,3 +19,7 @@ def get_sum_of_multiples_using_loop(maximum):
             sum_of_multiples += i
 
     return sum_of_multiples
+
+
+def get_sum_of_multiples_using_list_comprehension(maximum):
+    return sum([i for i in range(1, maximum) if i % 5 == 0 or i % 3 == 0])
--- tests/p001_tests.py
+++ tests/p001_tests.py
@@ -6,3 +6,9 @@ def test_loop_version():
     sum_of_multiples_for_10 = get_sum_of_multiples_using_loop(10)
 
     assert_equal(sum_of_multiples_for_10, 23)
+
+
+def test_list_comprehension_version():
+    sum_of_multiples_for_10 = get_sum_of_multiples_using_list_comprehension(10)
+
+    assert_equal(sum_of_multiples_for_10, 23)
