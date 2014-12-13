5d6bee3638b772363168402ad11beda341494538
wpkita <william.p.kita@gmail.com>
2014-12-12 19:38:56 -0800

    Scaffold out new solutions using scaffold.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-project-euler-python
-====================
+Project Euler Solutions - Python 3.x
+====================================
+
+Scaffolding a new solution
+--------------------------
+Use the `scaffold.py` file at the root to quickly generate the skeleton for a new solution. The only parameter the script accepts is an integer corresponding to the problem number.
+
+For example: `python3 scaffold.py 11` will generate a folder named **p011** and a file for tests in the **tests** folder.
---
+++ scaffold.py
@@ -0,0 +1,25 @@
+import argparse
+import shutil
+import fileinput
+
+# Configure and read command line arguments
+parser = argparse.ArgumentParser(description='Scaffold out directory/file structure for new Project Euler problem.')
+parser.add_argument('problem_number', type=int)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    problem_number = args.problem_number
+
+    # e.g. 6 becomes 'p006'
+    problem_label = 'p{0:03d}'.format(problem_number)
+
+    # Copy template files into p### directory
+    shutil.copytree('problem_template', '{0}'.format(problem_label))
+
+    # Move tests file into tests directory, rename as p###_tests.py
+    tests_file_name = 'tests/{0}_tests.py'.format(problem_label)
+    shutil.move('{0}/tests.py'.format(problem_label), tests_file_name)
+
+    # Fix reference in tests file to point to p### package
+    for line in fileinput.input(tests_file_name, inplace=True):
+        print(line.replace('problem', problem_label), end='')
