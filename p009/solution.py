4b006eb29375deb71d6a500d50efe8d074500b42
wpkita <william.p.kita@gmail.com>
2014-12-13 23:32:24 -0800

    Solve p009 with Euclid's formula for generating Pythagorean Triples

--- p009/solution.py
+++ p009/solution.py
@@ -1,2 +1,20 @@
+from functools import reduce
+from operator import mul
+
+
+def get_pythagorean_triple_summing_to(pythagorean_sum):
+    for m in range(2, pythagorean_sum):
+        for n in range(1, m):
+            a = m ** 2 - n ** 2
+            b = 2 * m * n
+            c = m ** 2 + n ** 2
+
+            if a + b + c == pythagorean_sum:
+                return a, b, c
+
+
 if __name__ == '__main__':
-    print()
+    pythagorean_triple = get_pythagorean_triple_summing_to(1000)
+    product_of_triple = reduce(mul, pythagorean_triple)
+
+    print(product_of_triple)
--- tests/p009_tests.py
+++ tests/p009_tests.py
@@ -1,2 +1,7 @@
 from nose.tools import *
 from p009.solution import *
+
+
+def test_get_pythagorean_triple_summing_to():
+    assert_equal(get_pythagorean_triple_summing_to(12), (3, 4, 5))
+    assert_equal(get_pythagorean_triple_summing_to(30), (5, 12, 13))
