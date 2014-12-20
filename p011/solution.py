852bd844bc70ce2203d368399bb7fb592ee829d3
wpkita <william.p.kita@gmail.com>
2014-12-19 22:03:41 -0800

    Solve p011

--- p011/solution.py
+++ p011/solution.py
@@ -1,2 +1,49 @@
+from functools import reduce
+from operator import mul
+
+
+def product(lst):
+    return reduce(mul, lst)
+
+
+def get_highest_product_in_grid(grid, group_length):
+    # Assumes square grid
+    dimension = len(grid)
+
+    if group_length > dimension:
+        return -1
+
+    max_product = 0
+
+    # Iterate over rows
+    for row in grid:
+        for col in range(dimension - (group_length - 1)):
+            lst = row[col:col+group_length]
+            max_product = max(max_product, product(lst))
+
+    # Iterate over columns
+    for row in range(dimension - (group_length - 1)):
+        for col in range(dimension):
+            lst = []
+            for offset in range(group_length):
+                lst.append(grid[row + offset][col])
+            max_product = max(max_product, product(lst))
+
+    # Iterate over diagonals
+    for row in range(dimension - (group_length - 1)):
+        for col in range(dimension - (group_length - 1)):
+            lst = []
+            for offset in range(group_length):
+                lst.append(grid[row + offset][col + offset])
+            max_product = max(max_product, product(lst))
+
+    return max_product
+
 if __name__ == '__main__':
-    print()
+    number_grid = []
+
+    with open('number-grid.txt', 'r') as number_grid_file:
+        for line in number_grid_file:
+            number_grid.append([int(number) for number in line.strip().split()])
+
+    print(get_highest_product_in_grid(number_grid, 4))
--- tests/p011_tests.py
+++ tests/p011_tests.py
@@ -1,2 +1,13 @@
 from nose.tools import *
 from p011.solution import *
+
+
+def test_get_highest_product_in_grid():
+    grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert_equal(get_highest_product_in_grid(grid, 2), 72)
+
+    grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert_equal(get_highest_product_in_grid(grid, 3), 504)
+
+    grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert_equal(get_highest_product_in_grid(grid, 4), -1)
